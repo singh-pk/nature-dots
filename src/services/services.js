d196a97e4ff311c3fc8d5215ef709c5a461d6cee
singh-pk <pratik@greedygame.com>
2021-06-15 23:46:05 +0530

    Optimizing for deployment

--- src/services/services.js
+++ src/services/services.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: 'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com',
+});
+
 export const getStations = () =>
-  axios.get('/v2/locations?city=London&parameter=no2&parameter=o3');
+  instance.get('/v2/locations?city=London&parameter=no2&parameter=o3');
